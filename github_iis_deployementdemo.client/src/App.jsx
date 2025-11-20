944f58aaf479a816c7dd03316993c0564b092c05
Harsh76200 <patkaravinash9@gmail.com>
2025-11-20 20:26:54 +0530

    ddf

--- github_iis_deployementdemo.client/src/App.jsx
+++ github_iis_deployementdemo.client/src/App.jsx
@@ -16,10 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Tanu.....Harsh</h1>
-      <h1>Tanu.....Harsh</h1>
-      <h1>Tanu.....Harsh</h1>
-      <h1>Tanu.....Harsh</h1>
+      <h1></h1>
+      <h1>Cognizenntttt</h1>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
